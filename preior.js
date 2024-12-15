2c9acce03fdedd79528437eecbb66f3bf0501fa2
Thu Thu San <132329019+Mei290900@users.noreply.github.com>
2024-12-15 21:57:06 +0630

    second part

---
+++ preior.js
@@ -0,0 +1,8 @@
+// infinte rotate the carousel
+$(document).ready(function() {
+    $('#carouselExampleDark').carousel({
+      interval: 5000, // Adjust the interval as needed
+      cycle: true
+    });
+  });
+
